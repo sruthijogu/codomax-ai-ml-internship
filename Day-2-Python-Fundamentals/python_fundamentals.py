54991da5686b07b7692f33211ee76d3899507409
sruthijogu <sruthijogu05@gmail.com>
2026-08-14 22:35:43 +0530

    Create Day-2-Python-Fundamentals/python_fundamentals.py

---
+++ Day-2-Python-Fundamentals/python_fundamentals.py
@@ -0,0 +1,42 @@
+# Day 2 - Python Fundamentals
+
+# Variables
+name = "Sruthi"
+age = 19
+
+print("Name:", name)
+print("Age:", age)
+
+
+# Data Types
+name = "Sruthi"
+age = 19
+height = 5.5
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+
+# Arithmetic Operations
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+
+# User Input
+name = input("Enter your name: ")
+print("Hello", name)
+
+
+# List
+subjects = ["Python", "AI", "Machine Learning", "Data Analytics"]
+
+print("Subjects:", subjects)
+print("Number of subjects:", len(subjects))
