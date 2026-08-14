1094a928286ca7e7a55a3a685777008df8414aca
sruthijogu <sruthijogu05@gmail.com>
2026-08-14 22:32:30 +0530

    Create Day-4-Functions-and-Lists/functions.py

---
+++ Day-4-Functions-and-Lists/functions.py
@@ -0,0 +1,25 @@
+# Day 4 - Functions
+
+def greet():
+    print("Hello, Python!")
+
+greet()
+
+
+def greet_name(name):
+    print("Hello", name)
+
+greet_name("Sruthi")
+
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print("Sum:", result)
+
+
+def square(number):
+    return number * number
+
+print("Square:", square(5))
